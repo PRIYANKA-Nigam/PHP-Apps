f01e39eb962bdd8b2c7b9f37d6d993fca1a03761
PRIYANKA-Nigam <nigampriyanka042@gmail.com>
2023-04-26 16:35:23 +0530

    test commit

--- gtl/Banking App/demoApp/members.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("../connection.php");
-if(isset($_SESSION['username']) && isset($_SESSION['id'])){
-    $sql="select * from users order by id asc";
-    $res=mysqli_query($con,$sql);
-}else{
-    header("Location:index.php");
-}
-?>
